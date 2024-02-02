a35edcd653dc00012dd3cc5c8a7792b90a794a4f
Thiago Miranda de Paula <155387074+dTMP3st@users.noreply.github.com>
2024-02-02 13:56:09 -0300

    Create DiscoverNET.sh
    Creation of the project's main code.

---
+++ DiscoverNET.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+clear
+echo ""
+
+Banner (){
+echo "#######################"
+echo "## IoT Alive Scanner ##"
+echo "#######################"
+echo ""
+}
+
+Banner
+
+if [ "$1" == "" ]; then
+	echo "	[-] - Use mode => $0 <API_Key>"
+else
+	while true
+	do
+		Exec_Hour=$(date | cut -d " " -f5)
+		# Time definition - Está funcionando - Adequar hora do servidor.
+		if [ "$Exec_Hour" == "00:00:00" ] || [ "$Exec_Hour" == "08:00:00" ] || [ "$Exec_Hour" == "16:00:00" ] || [ "$Exec_Hour" == "16:36:00" ]; then
+			Shodan_Start=$(shodan init $1)
+			if [ "$Shodan_Start" == "Successfully initialized" ]; then
+				echo "	[+] Shodan start ..."
+				for Target_Query in $(cat Target_Query.txt); do
+					echo "		[+] Search by $Target_Query ..."
+					shodan search $Target_Query --fields ip_str country:BR 2> /dev/null | sort -u >> Shodan_IPList.txt
+					# Tentar realizar parsing. shodan search $Target_Query parse --fields ip_str,port --separator , | cut -d "," f1 | sort -u > Shodan_IPList.txt
+				done
+					for IP in $(cat Shodan_IPList.txt); do
+						Ports=$(shodan search $IP --fields port country:BR >> Shodan_PortList.txt)
+						echo "	[+] IP => $IP | Ports => $Ports"
+					done
+					# Apenas para listar IPs - cat Shodan_IPList.txt
+					# Versão final -  tail -f Shodan_IPList.txt
+			else
+				echo "	[-] Shodan failure ..."
+				exit
+			fi
+		else
+			echo "	[-] - Wait time to execute ..." >> /dev/null
+		fi
+	done
+fi
+
+# Criar regra de detecção.
+# Equipamento exposto para a Internet mapeado pelo Shodan.

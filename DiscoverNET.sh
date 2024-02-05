8f539a93fa4347cab3dba6658114ab2eaeec6c5c
dTMP3st <ThiagoMirandaDePaula@gmail.com>
2024-02-05 11:28:51 -0300

    Change in main code.

--- DiscoverNET.sh
+++ DiscoverNET.sh
@@ -19,18 +19,22 @@ else
 	do
 		Exec_Hour=$(date | cut -d " " -f5)
 		# Time definition - Está funcionando - Adequar hora do servidor.
-		if [ "$Exec_Hour" == "00:00:00" ] || [ "$Exec_Hour" == "08:00:00" ] || [ "$Exec_Hour" == "16:00:00" ] || [ "$Exec_Hour" == "16:36:00" ]; then
+		if [ "$Exec_Hour" == "00:00:00" ] || [ "$Exec_Hour" == "08:00:00" ] || [ "$Exec_Hour" == "16:00:00" ] || [ "$Exec_Hour" == "00:43:00" ]; then
 			Shodan_Start=$(shodan init $1)
 			if [ "$Shodan_Start" == "Successfully initialized" ]; then
 				echo "	[+] Shodan start ..."
 				for Target_Query in $(cat Target_Query.txt); do
 					echo "		[+] Search by $Target_Query ..."
 					shodan search $Target_Query --fields ip_str country:BR 2> /dev/null | sort -u >> Shodan_IPList.txt
-					# Tentar realizar parsing. shodan search $Target_Query parse --fields ip_str,port --separator , | cut -d "," f1 | sort -u > Shodan_IPList.txt
 				done
 					for IP in $(cat Shodan_IPList.txt); do
-						Ports=$(shodan search $IP --fields port country:BR >> Shodan_PortList.txt)
-						echo "	[+] IP => $IP | Ports => $Ports"
+						Port=$(shodan host $IP --format tsv)
+						echo "	[+] IP => $IP"
+						echo "	[+] Ports:"
+						echo "		$Port"
+						echo ""
+						echo ""
+						echo ""
 					done
 					# Apenas para listar IPs - cat Shodan_IPList.txt
 					# Versão final -  tail -f Shodan_IPList.txt

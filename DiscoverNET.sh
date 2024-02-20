4aedc7666632ca470b69847547103a292a6bf8a8
dTMP3st <ThiagoMirandaDePaula@gmail.com>
2024-02-20 11:24:27 -0300

    Updated code.

--- DiscoverNET.sh
+++ DiscoverNET.sh
@@ -17,7 +17,7 @@ if [ "$1" == "" ]; then
 else
 	while true
 	do
-		Exec_Hour=$(date | cut -d " " -f5)
+		Exec_Hour=$(date | cut -d " " -f4) # Em alguns casos pode ser -f5 o campo.
 		# Time definition - Está funcionando - Adequar hora do servidor.
 		if [ "$Exec_Hour" == "00:00:00" ] || [ "$Exec_Hour" == "08:00:00" ] || [ "$Exec_Hour" == "16:00:00" ] || [ "$Exec_Hour" == "$2" ]; then
 			Shodan_Start=$(shodan init $1)
@@ -25,10 +25,10 @@ else
 				echo "	[+] Shodan start ..."
 				for Target_Query in $(cat Target_Query.txt); do
 					echo "		[+] Search by $Target_Query ..."
-					shodan search $Target_Query --fields ip_str country:BR 2> /dev/null | sort -u >> Shodan_IPList.txt
+					shodan search $Target_Query --fields ip_str country:BR 2> /dev/null >> Shodan_IPList.txt
 				done
-					for IP in $(cat Shodan_IPList.txt); do
-						Port=$(shodan host $IP --format tsv)
+					for IP in $(cat Shodan_IPList.txt | sort -u); do
+						Port=$(shodan host $IP --format tsv | cut -d " " -f5,6)
 						echo "	[+] IP => $IP"
 						echo "	[+] Ports:"
 						echo "		$Port"
@@ -38,10 +38,16 @@ else
 					done
 					# Apenas para listar IPs - cat Shodan_IPList.txt
 					# Versão final -  tail -f Shodan_IPList.txt
+			elif [ "$Exec_Hour" == "23:00:00" ]; then
+				# Execução do scan do Shodan.
+				# shodan scan submit
 			else
 				echo "	[-] Shodan failure ..."
 				exit
 			fi
+			# Insert Censys code.
+
+			if [  ]
 		else
 			echo "	[-] - Wait time to execute ..." >> /dev/null
 		fi
